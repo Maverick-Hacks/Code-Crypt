e96686d00fe26144d8cdd4a0f6d3cb39f92138d3
Abhinand Arun <abhinandarun2015@gmail.com>
2023-11-24 21:25:14 +0530

    fix: remove unwanted file

--- frontend/config/fonts.ts
+++ frontend/config/fonts.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Fira_Code as FontMono, Inter as FontSans, Poppins as FontPoppins } from "next/font/google"
-=======
 import { Fira_Code as FontMono, Inter as FontSans, Poppins as FontPoppins } from 'next/font/google'
->>>>>>> dev
 
 export const fontSans = FontSans({
   subsets: ['latin'],
@@ -36,10 +32,4 @@ export const fontPoppinsLight = FontPoppins({
   weight: '300',
   subsets: ['latin'],
   variable: '--font-poppins-light',
-})
-
-export const fontPoppins = FontPoppins({
-  subsets: ["latin"],
-  variable: "--font-mono",
-  weight: "700"
 })
